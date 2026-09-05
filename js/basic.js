9f9e8931d81b3e2e16292cb0e1165867960ba7e8
Frici <friedmann.mate0329@gmail.com>
2026-09-05 21:52:26 +0200

    Leírás javítása és scriptek bekötése

--- js/basic.js
+++ js/basic.js
@@ -1,10 +1,12 @@
+var points = "Working Souls - Főoldal" == document.title ? ".":".."
+
 var icon = document.querySelector("link[rel~='icon']") // Ha van már icon akkor azt kötjük a változóhoz
 if (!icon) { // ha nincs link element kötve a változóhoz akkor létrehozunk egyet
     icon = document.createElement('link')
     icon.rel = 'icon'
     document.head.appendChild(icon)
 }
-icon.href = "./imgs/icon.ico" // beállítjuk az ikon képet
+icon.href = `${points}/imgs/icon.ico` // beállítjuk az ikon képet
 
 var basicCSS = document.querySelector("link[rel~='stylesheet'][href='./css/basic.css']") // lekérjük a közös css bekötését
 if (!basicCSS) {
@@ -12,7 +14,7 @@ if (!basicCSS) {
     basicCSS.rel = "stylesheet"
     document.head.appendChild(basicCSS)
 }
-basicCSS.href = "./css/basic.css"
+basicCSS.href = `${points}/css/basic.css`
 
 
 document.body.innerHTML += `
@@ -21,9 +23,9 @@ document.body.innerHTML += `
         <h2></h2>
     </header>
     <menu>
-        <li><a href="?page=home">Főoldal</a></li>
-        <li><a href="?page=register">Regisztráció</a></li>
-        <li><a href="?page=login">Bejelentkezés</a></li>
-        <li><a href="?page=profil">Profil</a></li>
-        <li><a href="?page=stat">Statisztikák</a></li>
+        <li><a href="${points}/index.html">Főoldal</a></li>
+        <li><a href="${points}/pages/register.html">Regisztráció</a></li>
+        <li><a href="${points}/pages/login.html">Bejelentkezés</a></li>
+        <li><a href="${points}/pages/profil.html">Profil</a></li>
+        <li><a href="${points}/pages/stat.html">Statisztikák</a></li>
     </menu>`
